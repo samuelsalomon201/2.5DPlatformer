e9ccc13df2d53005e60daa18fc8da7945dad751a
samuelsalomon201 <84681310+samuelsalomon201@users.noreply.github.com>
2021-06-18 12:28:32 -0400

    Added all the files
    Added all the files

---
+++ Assets/Scripts/Portal1.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private Text _coinText, _livesText;
+
+    public void UpdateCoinDisplay(int coins)
+    {
+        _coinText.text = "Coins: " + coins;
+    }
+
+    public void UpdateLivesDisplay(int lives)
+    {
+        _livesText.text = "Lives: " + lives;
+    }
+}
